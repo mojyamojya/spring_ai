12f2dd9bc74d49d1353b93eef8ec5cf95bcd80cf
Akihiko Sato <asato@live-cast.asia>
2025-12-16 10:22:22 +0900

    プロンプトテンプレートを使うように修正

--- simple_chat/src/main/java/com/example/simple_chat/SimpleChatService.java
+++ simple_chat/src/main/java/com/example/simple_chat/SimpleChatService.java
@@ -1,6 +1,10 @@
 package com.example.simple_chat;
 
+// import java.util.Map;
+
 import org.springframework.ai.chat.client.ChatClient;
+// import org.springframework.ai.chat.prompt.Prompt;
+// import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +16,28 @@ public class SimpleChatService {
 
     public String sendPrompt(String message) {
         ChatClient client = builder.build();
-        ChatClient.CallResponseSpec response = client.prompt().user(message).call();
+        // PromptTemplate template = new PromptTemplate("""
+        //     あなたは年間100人以上のエンジニアを育てるプログラミング講師です。
+        //     未経験・初心者にもわかるよう質問に丁寧に回答してください。
+        //     箇条書きでポイントをまとめてください。
+        //     -----
+        //     受講生からの質問：{question}
+        // """);
+        // Prompt prompt = template.create(Map.of("question", message));
+        ChatClient.CallResponseSpec response = client.prompt()
+            .system("""
+                あなたは年間100人以上のエンジニアを育てるプログラミング講師です。
+                未経験・初心者にもわかるよう質問に丁寧に回答してください。
+                箇条書きでポイントをまとめてください。
+                わからない場合は「わからない」と答えてください。                
+            """)
+            .user("""
+                受講生からの質問：
+                -----
+                %s
+            """.formatted(message)).call();
+        // ChatClient.CallResponseSpec response = client.prompt(prompt).call();
+        // ChatClient.CallResponseSpec response = client.prompt().user(message).call();
         return response.content();
     }
 }
